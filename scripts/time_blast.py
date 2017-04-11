ef6e7cebc2cff63c23d15ce8495b5cac41acdb91
Daneil Buchan <clone@myself.com>
2017-04-11 16:18:13 +0100

    blast timing script

--- scripts/time_blast.py
+++ scripts/time_blast.py
@@ -24,8 +24,7 @@ def run_blast(file):
         p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         p.wait()
-        print(seq_name+" : "+str(p.stdout.read()))
-        print(seq_name+" : "+str(p.stderr.read()))
+        print(seq_name+" : "+p.stderr.read().decode())
 
 
 # fasta= open("pdb_2015.fasta", "w")
