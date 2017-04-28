3422c611004c6e7fe22bed1701b446c3ab83599e
Daneil Buchan <clone@myself.com>
2017-04-28 17:44:01 +0100

    pass numbering in concurrent

--- scripts/time_concurrent_cores.py
+++ scripts/time_concurrent_cores.py
@@ -12,8 +12,8 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 def run_blast(data):
-    file_count = data.split("_", 1)[1]
-    file_name = data.split("_", 1)[0]
+    file_count = data.split(":", 1)[1]
+    file_name = data.split(":", 1)[0]
     seq_path = file_name[0:-6]
     # print(seq_path)
     seq_name = seq_path.split("/")[-1]
@@ -40,7 +40,7 @@ print("seq,cores/concurrency,type,time_output")
 for i in range(5, int(sys.argv[2])+1):
     process_list = []
     for j in range(1, i+1):
-        process_list.append(seq+"_"+str(j))
+        process_list.append(seq+":"+str(j))
     start_time = time.time()
     p = Pool(i)
     p.map(run_blast, process_list)
